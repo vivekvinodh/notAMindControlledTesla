38195e7cda2cac98c351902ab94362f8627eadf5
vivekvinodh <vvino001@ucr.edu>
2016-11-12 21:54:16 -0800

    hard code to cu

--- eeg/inference.py
+++ eeg/inference.py
@@ -12,18 +12,18 @@ from util import open_bci
 
 if __name__ == '__main__':
   assert len(sys.argv) == 2, "Must provide filename as argument."
-  
+
   # Read in file.
   data = train.read_clean(sys.argv[1], 'go', 'stop')
-  
+
   # Split into train/test
   Xtr, Xte, Ytr, Yte = train.prepare_train_test(data)
-  
+
   # Prepare submodels
   ica = decomposition.FastICA(n_components=6)
   nn = neighbors.KNeighborsClassifier(n_neighbors=3)
   model = train.TransformClassifier(ica, nn)
-  
+
   # Train model
   model.fit(Xtr, Ytr)
   print("\nModel score: {}\n".format(model.score(Xte, Yte)))
@@ -33,7 +33,7 @@ if __name__ == '__main__':
   # ports = open_bci.find_ports()
   # print("Available ports: {}".format(ports))
   bci_port = '/dev/tty.usbserial-DQ007SU3'
-  arduino_port = '/dev/tty.usbmodem1411' 
+  arduino_port = '/dev/cu.usbmodem1411' 
 
   print("Starting connection with OpenBCI on port={}...".format(bci_port))
   board = open_bci.OpenBCIBoard(port=bci_port)
