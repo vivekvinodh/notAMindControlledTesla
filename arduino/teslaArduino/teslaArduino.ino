b7e3166fa11a03a852de729eefaaeeb8dc8df92f
vivekvinodh <vvino001@ucr.edu>
2016-11-13 09:44:04 -0800

    remove delays

--- arduino/teslaArduino/teslaArduino.ino
+++ arduino/teslaArduino/teslaArduino.ino
@@ -2,13 +2,15 @@
 #define PULSE 300
 
 const int outPin = 10;
-const int DOWN = 1300;
-const int UP = 1100;
+const int RETRACT = 1300;
+const int EXTEND = 1100;
 const int ERR = 1700;
+const int RETRACTGAS = 1700;
+const int FORCEBRAKE = 900;
 
 int channels[6] = {1200, 1200, 1200, 1200, 1200, 1200};
-//channels[0] = CH1 - RELIABLE 1200-FLAT 1300-DOWN 1100-UP
-//channels[1] = CH2 - RELIABLE 1200-FLAT 1300-DOWN 1100-UP
+//channels[0] = CH1/BRAKE - RELIABLE 1200-FLAT (x>1300)-RETRACT (x<1100)-EXTEND 
+//channels[1] = CH2/GAS - RELIABLE 1200-FLAT (x>1300)-RETRACT (x<1100)-EXTEND
 //channels[2] = CH3
 //channels[3] = CH4
 //channels[4] = CH5
@@ -74,8 +76,8 @@ void readBuf(){
     if(atoi(this_line.c_str()) == 0){
       //update channel
       Serial.println("stop");
-      scaleUp(1,channels[1],DOWN);
-      scaleDown(0,channels[0],UP);
+      scaleUp(1,channels[1],RETRACT);
+      scaleDown(0,channels[0],EXTEND-100);
       //channels[1] = DOWN;
       //channels[0] = UP;
     }
@@ -83,14 +85,17 @@ void readBuf(){
       if(atoi(this_line.c_str()) == 1){
         //update channel
         Serial.println("go");
-        scaleDown(1, channels[1], UP);
-        scaleUp(0, channels[0], DOWN);
+        scaleDown(1, channels[1], EXTEND);
+        scaleUp(0, channels[0], RETRACT+100);
         //channels[1] = UP;
         //channels[0] = DOWN;
       }
       else{
         //assume crit fail, reset actuators
+        Serial.println("err");
         channels[3] = ERR;
+        channels[0] = FORCEBRAKE;
+        channels[1] = RETRACTGAS;
       }
     }
   }
